887e776c7bfccd2f7ebdbee41dabeb3aa695a0dd
Karine Samyn <Karine.Samyn@hogent.be>
2019-09-20 09:18:49 +0200

    Add class BankAccount

---
+++ Banking/Models/Domain/BankAccount.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Banking.Models.Domain
+{
+    class BankAccount
+    {
+        #region Properties
+        public decimal Balance { get; private set; }
+
+        public string AccountNumber { get; }
+        #endregion
+
+        #region Constructors
+        public BankAccount(string account)
+        {
+            AccountNumber = account;
+            Balance = Decimal.Zero;
+        }
+        #endregion
+
+        #region Methods
+        public void Withdraw(decimal amount)
+        {
+            Balance -= amount;
+        }
+
+        public void Deposit(decimal amount)
+        {
+            Balance += amount;
+        }
+        #endregion
+    }
+}
--- Banking/Program.cs
+++ Banking/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Banking.Models.Domain;
+using System;
 
 namespace Banking
 {
@@ -6,7 +7,13 @@ namespace Banking
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            BankAccount account = new BankAccount("123-4567890-02");
+            Console.WriteLine($"AccountNumber: {account.AccountNumber} ");
+            Console.WriteLine($"Balance: {account.Balance} ");
+            account.Deposit(200M);
+            Console.WriteLine($"Balance after deposit of 200 euros: {account.Balance} ");
+            account.Withdraw(100);
+            Console.WriteLine($"Balance after withdraw of 100 euros: {account.Balance} ");
         }
     }
 }
